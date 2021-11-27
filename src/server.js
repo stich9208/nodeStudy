7bcec8f7278edbb830403a00cfef012a1f111beb
JAEWOOKJUNG <stich9208@seerslab.com>
2021-11-27 21:40:58 +0900

    Add: first server open
    Signed-off-by: JAEWOOKJUNG <stich9208@seerslab.com>

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log("this is server");
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+const handleListen = () => {
+  console.log(`🌐 port ${PORT} is open!!`);
+};
+
+const handleGet = (req, res) => {
+  res.send("<h1>Hello Node!</h1>");
+};
+
+app.get("/", handleGet);
+
+app.listen(PORT, handleListen);
