09d30dd0a552bbfb808df9ac580b1361b521d211
JAEWOOKJUNG <stich9208@seerslab.com>
2021-11-28 22:04:09 +0900

    Add: middleware 추가 테스트
    Signed-off-by: JAEWOOKJUNG <stich9208@seerslab.com>

--- src/server.js
+++ src/server.js
@@ -2,16 +2,33 @@ import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 4000;
 
+//controller
 const handleListen = () => {
   console.log(`🌐 port ${PORT} is open!!`);
 };
-
-const handleGet = (req, res) => {
-  res.send("<h1>Hello Node!</h1>");
+const handleHome = (req, res) => {
+  res.send("<h1>HOME!</h1>");
+};
+const handleLogin = (req, res) => {
+  res.send("<h1>LOGIN!</h1>");
 };
 
-app.get("/", handleGet);
+//middleware
+const logMethod = (req, res, next) => {
+  console.log(`METHOD: ${req.method}`);
+  next();
+};
+const logPath = (req, res, next) => {
+  console.log(`PATH: ${req.path}`);
+  next();
+};
+
+//use middleware globally
+app.use(logMethod, logPath);
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 app.listen(PORT, handleListen);
